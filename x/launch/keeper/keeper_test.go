d48e8d88dc0d86939301c0c05892d598179aa831
aljo242 <alex.johnson@tendermint.com>
2022-02-14 11:33:17 -0500

    add launch keeper field

--- x/launch/keeper/keeper_test.go
+++ x/launch/keeper/keeper_test.go
@@ -1,10 +1,15 @@
 package keeper_test
 
 import (
+	"testing"
+	"time"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/suite"
-
 	"github.com/tendermint/spn/app"
+	"github.com/tendermint/spn/x/launch/keeper"
+
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
 type KeeperTestSuite struct {
@@ -12,4 +17,17 @@ type KeeperTestSuite struct {
 
 	ctx sdk.Context
 	app *app.App
+
+	k keeper.Keeper
+}
+
+func (suite *KeeperTestSuite) SetupTest() {
+	suite.app = app.Setup(false)
+	suite.ctx = suite.app.NewContext(false, tmproto.Header{Height: 1, ChainID: "spn-1", Time: time.Now().UTC()})
+
+	suite.k = suite.app.LaunchKeeper
+}
+
+func TestKeeperTestSuite(t *testing.T) {
+	suite.Run(t, new(KeeperTestSuite))
 }
